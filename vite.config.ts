bf5014a51730a3a6e324646cf32a3d1e6e4dd949
Duye <superdoye0120@outlook.com>
2025-05-27 17:53:40 +0800

    shadcn 初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from '@tailwindcss/vite';
-import { fileURLToPath, URL } from 'node:url';
+import path from "path";
 import { defineConfig } from 'vite';
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
